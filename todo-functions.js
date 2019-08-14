4f30cfb5fd01d3c4ca6f75c2b1485a5b05ff46ee
LuigiDerson <luigi.prodesign@gmail.com>
2019-08-14 22:12:07 +0200

    Try & catch on getSavedTodos() function

--- todo-functions.js
+++ todo-functions.js
@@ -2,7 +2,12 @@
 const getSavedTodos = () => {
   const todosJSON = localStorage.getItem("todos");
 
-  return todosJSON ? JSON.parse(todosJSON) : [];
+  try {
+    return todosJSON ? JSON.parse(todosJSON) : [];
+  } catch(e) {
+    return [];
+  }
+
 }
 
 // Save todos to localStorage
